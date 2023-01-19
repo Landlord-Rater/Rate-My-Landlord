01fb38a9235a9aa2db0afd2ea92660f9771ece30
michellesylee <michellebellelee@yahoo.com>
2023-01-19 10:17:25 -0800

    editing controllers

--- backend/controllers/dbController.js
+++ backend/controllers/dbController.js
@@ -1,18 +1,18 @@
-const db = require("../models");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const db = require('../models');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
-require("dotenv").config();
+require('dotenv').config();
 
 const dbController = {};
 
 dbController.getAll = (req, res, next) => {
-  const text = "SELECT * FROM landlords";
+  const text = 'SELECT * FROM landlords';
 
   db.query(text)
-    .then(async data => {
+    .then(async (data) => {
       const queryText =
-        "SELECT AVG(rating) FROM reviews where landlord_id = $1;";
+        'SELECT AVG(rating) FROM reviews where landlord_id = $1;';
       const landLords = data.rows;
       //this loop is to query each landlord's review and find the average of all their ratings
       for (const person of landLords) {
@@ -25,9 +25,9 @@ dbController.getAll = (req, res, next) => {
       }
       res.status(200).json(landLords);
     })
-    .catch(err =>
+    .catch((err) =>
       next({
-        log: "error caught in getAll middleware!",
+        log: 'error caught in getAll middleware!',
         status: 400,
         message: { err: err },
       })
@@ -37,14 +37,14 @@ dbController.getAll = (req, res, next) => {
 dbController.createLandlord = (req, res, next) => {
   const { location, name } = req.body;
   const text =
-    "INSERT INTO landlords(name, location) VALUES ($1,$2) RETURNING _id, name, location";
+    'INSERT INTO landlords(name, location) VALUES ($1,$2) RETURNING _id, name, location';
 
   const value = [name, location];
   db.query(text, value)
-    .then(data => res.json(data.rows[0]))
-    .catch(err =>
+    .then((data) => res.json(data.rows[0]))
+    .catch((err) =>
       next({
-        log: "error caught in createLandLord middleware!",
+        log: 'error caught in createLandLord middleware!',
         status: 400,
         message: { err: err },
       })
@@ -54,103 +54,46 @@ dbController.createLandlord = (req, res, next) => {
 dbController.getLandLord = (req, res, next) => {
   const text =
     // "select landlords.name, landlords.location, reviews.rating, reviews.would_rent_again, reviews.landlord_id as _id from reviews right join landlords ON landlords._id = $1 AND reviews.landlord_id = $1";
-    "SELECT landlords.name, landlords.location, reviews.rating, reviews.would_rent_again, reviews.landlord_id AS _id FROM landlords LEFT JOIN reviews ON landlords._id =  reviews.landlord_id WHERE landlords._id = $1";
+    'SELECT landlords.name, landlords.location, reviews.rating, reviews.would_rent_again, reviews.landlord_id AS _id FROM landlords LEFT JOIN reviews ON landlords._id =  reviews.landlord_id WHERE landlords._id = $1';
   const landlord = req.params.id;
   console.log(landlord);
   const value = [landlord];
   db.query(text, value)
-    .then(data => {
+    .then((data) => {
       //redirect to landlord submission page if landlord is not found
       console.log(data.rows);
-      if (!data.rows[0]) return res.json("landlord not in database");
+      if (!data.rows[0]) return res.json('landlord not in database');
       // ratings is the average of all the ratings from all reviews
       // rentAgain is the average of 1s and 0s from the would_rent_again column in all reviews
-      const ratings = average(data.rows.map(el => el["rating"]));
-      const rentAgain = average(data.rows.map(el => el["would_rent_again"]));
-      data.rows[0]["rating"] = Number.parseFloat(ratings).toFixed(1);
-      data.rows[0]["would_rent_again"] = `${rentAgain * 100}%`;
+      const ratings = average(data.rows.map((el) => el['rating']));
+      const rentAgain = average(data.rows.map((el) => el['would_rent_again']));
+      data.rows[0]['rating'] = Number.parseFloat(ratings).toFixed(1);
+      data.rows[0]['would_rent_again'] = `${rentAgain * 100}%`;
       //pass the landlord card with name of landlord and the average ratings
       res.locals.landLord = data.rows[0];
       next();
     })
-    .catch(err =>
+    .catch((err) =>
       next({
-        log: "error caught in getLandLord middleware!",
+        log: 'error caught in getLandLord middleware!',
         status: 400,
         message: { err: err },
       })
     );
-};
-
-dbController.createUsers = async (req, res, next) => {
-  const { username, email, password } = req.body;
-
-  db.query(`SELECT * FROM users where email = '${email}'`)
-    .then(data => {
-      if (data.rows[0] !== undefined)
-        return res.status(400).json({ error: "email has already been used" });
-    })
-    .catch(err =>
-      next({
-        log: "error caught in createUsers middleware while checking the existence of the account in the database!",
-        status: 400,
-        message: { err: err },
-      })
-    );
-
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-  const text = "INSERT INTO users(username,email,password) VALUES ($1,$2,$3)";
-
-  const value = [username, email, hashedPassword];
-
-  db.query(text, value)
-    .then(_ => res.status(200).json("user created"))
-    .catch(err =>
-      next({
-        log: "error caught in createUsers middleware while trying to insert new user data into database!",
-        status: 400,
-        message: { err: err },
-      })
-    );
-};
-
-dbController.getUsers = async (req, res, next) => {
-  const { email, password } = req.body;
-  const text = "SELECT * FROM users WHERE email = $1";
-  const value = [email];
-
-  const user = (await db.query(text, value)).rows[0];
-
-  try {
-    if (user && (await bcrypt.compare(password, user.password))) {
-      //pass the web token to the next middleware to set it as a cookie for session control
-      res.locals.id = generateToken({ id: user._id, username: user.username });
-      next();
-    } else {
-      res.json("email or password incorrect");
-    }
-  } catch (err) {
-    next({
-      log: "error caught in getUsers middleware!",
-      status: 400,
-      message: { err: err },
-    });
-  }
 };
 
 dbController.postReviews = (req, res, next) => {
   const { userId } = res.locals.user;
   const { landlord_id, text, rating, would_rent_again, date } = req.body;
   const queryText =
-    "INSERT INTO reviews (landlord_id, text, rating, would_rent_again, date, user_id) values($1,$2,$3,$4,$5,$6)";
+    'INSERT INTO reviews (landlord_id, text, rating, would_rent_again, date, user_id) values($1,$2,$3,$4,$5,$6)';
   const value = [landlord_id, text, rating, would_rent_again, date, userId];
 
   db.query(queryText, value)
-    .then(_ => res.status(200).json("review posted"))
-    .catch(err =>
+    .then((_) => res.status(200).json('review posted'))
+    .catch((err) =>
       next({
-        log: "error caught in postReviews middleware!",
+        log: 'error caught in postReviews middleware!',
         status: 400,
         message: { err: err },
       })
@@ -159,16 +102,16 @@ dbController.postReviews = (req, res, next) => {
 
 dbController.getReviews = (req, res, next) => {
   const text =
-    "SELECT reviews.*, users.username AS user FROM reviews INNER JOIN users ON reviews.user_id = users._id AND reviews.landlord_id = $1";
+    'SELECT reviews.*, users.username AS user FROM reviews INNER JOIN users ON reviews.user_id = users._id AND reviews.landlord_id = $1';
   const value = [res.locals.landLord._id];
   db.query(text, value)
-    .then(data => {
+    .then((data) => {
       res.locals.reviews = data.rows;
       next();
     })
-    .catch(err =>
+    .catch((err) =>
       next({
-        log: "error caught in getReviews middleware!",
+        log: 'error caught in getReviews middleware!',
         status: 400,
         message: { err: err },
       })
@@ -182,7 +125,7 @@ function average(arr) {
 
 function generateToken(id) {
   return jwt.sign(id, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: '30d',
   });
 }
 module.exports = dbController;
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const dbController = require("../controllers/dbController");
-const cookieController = require("../controllers/cookieController");
-const auth = require("../middleware/auth");
+const express = require('express');
+const dbController = require('../controllers/dbController');
+const userController = require('../controllers/userController');
+const cookieController = require('../controllers/cookieController');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
-router.get("/getall", dbController.getAll);
+router.get('/getall', dbController.getAll);
 
 router.get(
-  "/getlandlord/:id",
+  '/getlandlord/:id',
   dbController.getLandLord,
   dbController.getReviews,
   (req, res) => {
@@ -18,22 +19,22 @@ router.get(
   }
 );
 
-router.post("/createlandlord", dbController.createLandlord);
+router.post('/createlandlord', dbController.createLandlord);
 
 router.post(
-  "/login",
-  dbController.getUsers,
+  '/login',
+  userController.getUsers,
   cookieController.setSSIDCookie,
-  (req, res) => res.status(200).json("user authenicated!")
+  (req, res) => res.status(200).json('user authenicated!')
 );
 
-router.post("/logout", (req, res) => {
-  res.clearCookie("ssid");
-  res.status(200).json("logged out!");
+router.post('/logout', (req, res) => {
+  res.clearCookie('ssid');
+  res.status(200).json('logged out!');
 });
-router.post("/signup", dbController.createUsers);
+router.post('/signup', userController.createUsers);
 
-router.post("/postReviews", dbController.postReviews);
-router.post("/postReviews", auth.verifyToken, dbController.postReviews);
+router.post('/postReviews', dbController.postReviews);
+router.post('/postReviews', auth.verifyToken, dbController.postReviews);
 
 module.exports = router;
