6f0eea3ba872335247f1b338cc0fb7f28ef3ba4f
Jeff Pettis <jeffrey.pettis@gmail.com>
2023-01-19 19:33:52 -0800

    changed App to functional component

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,56 +1,45 @@
-import React, { Component } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './src/components/Navbar.jsx';
-import SearchPage from './src/components/SearchPage.jsx'; // characters
-import LandlordPage from './src/components/LandlordPage.jsx'; // customize character
-import Login from './src/components/Login.jsx';
-import Logout from './src/components/Logout.jsx';
-import Signup from './src/components/Signup.jsx';
-import AddLandlord from './src/components/AddLandlord.jsx';
-import ProfilePage from './src/components/ProfilePage.jsx';
-import PageNotFound from './src/components/PageNotFound.jsx';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./src/components/Navbar.jsx";
+import SearchPage from "./src/components/SearchPage.jsx"; // characters
+import LandlordPage from "./src/components/LandlordPage.jsx"; // customize character
+import Login from "./src/components/Login.jsx";
+import Logout from "./src/components/Logout.jsx";
+import Signup from "./src/components/Signup.jsx";
+import AddLandlord from "./src/components/AddLandlord.jsx";
+import ProfilePage from "./src/components/ProfilePage.jsx";
+import PageNotFound from "./src/components/PageNotFound.jsx";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    this.state = {
-      isLoggedIn: false,
-    };
-  }
-
-  updateLoginStatus = (isLoggedIn) => {
-    this.setState({ isLoggedIn });
+  const updateLoginStatus = (isLoggedIn) => {
+    setIsLoggedIn(isLoggedIn);
   };
 
-  render() {
-    return (
-      <div className='router'>
-        <main>
-          <Navbar isLoggedIn={this.state.isLoggedIn} />
-          <Routes>
-            <Route exact path='/' element={<SearchPage />} />
-            <Route exact path='/landlord' element={<LandlordPage />} />
-            <Route exact path='/profile' element={<ProfilePage />} />
-            <Route
-              exact
-              path='/login'
-              element={<Login updateLoginStatus={this.updateLoginStatus} />}
-            />
-            <Route
-              exact
-              path='/logout'
-              element={<Logout updateLoginStatus={this.updateLoginStatus} />}
-            />
-            <Route exact path='/signup' element={<Signup />} />
-            <Route exact path='/addlandlord' element={<AddLandlord />} />
-
-            <Route path='*' element={<PageNotFound />} />
-          </Routes>
-        </main>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="router">
+      <main>
+        <Navbar isLoggedIn={isLoggedIn} />
+        <Routes>
+          <Route exact path="/" element={<SearchPage />} />
+          <Route exact path="/landlord" element={<LandlordPage />} />
+          <Route
+            exact
+            path="/login"
+            element={<Login updateLoginStatus={updateLoginStatus} />}
+          />
+          <Route
+            exact
+            path="/logout"
+            element={<Logout updateLoginStatus={updateLoginStatus} />}
+          />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/addlandlord" element={<AddLandlord />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
