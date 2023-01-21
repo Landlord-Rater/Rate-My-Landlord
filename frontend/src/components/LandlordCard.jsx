25f52d70d9addbf459d18bcbcf0298c79639a7d9
Jeff Pettis <jeffrey.pettis@gmail.com>
2023-01-21 13:56:17 -0800

    saved merged changes

--- frontend/src/components/LandlordCard.jsx
+++ frontend/src/components/LandlordCard.jsx
@@ -1,18 +1,20 @@
 //renders multiple of these upon search in home page if they fit search criteria
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import House from '../assets/rundown house.jpeg'
+import React from "react";
+import { Link } from "react-router-dom";
+import House from "../assets/rundown house.jpeg";
 
 const LandlordCard = ({ landlord }) => {
-
   return (
     <div className="landlordCards p-4 rounded-md bg-primary text-white grid grid-cols-3 gap-10">
-
-      <img className="house object-fill col-span-1" src={House} alt="Logo" width="180" />
+      <img
+        className="house object-fill col-span-1"
+        src={House}
+        alt="Logo"
+        width="180"
+      />
 
       <div className="landlord-info col-span-2">
-
         <h2 className="landlord-name font-bold text-xl">{landlord.name}</h2>
 
         <div className="landlord-location">
@@ -23,10 +25,9 @@ const LandlordCard = ({ landlord }) => {
         <div className="landlord-rating">
           <div className="label font-medium mr-1">Rating: </div>
           <div className="info font-medium mr-1">
-          {landlord.averageRating ? landlord.averageRating : 'N/A'}
+            {landlord.averageRating ? landlord.averageRating : "N/A"}
           </div>
         </div>
-        
       </div>
 
       {/* <strong className="font-medium">Would Rent Again: </strong>
@@ -35,7 +36,7 @@ const LandlordCard = ({ landlord }) => {
       <div className="landlord-reviews-link-container flex justify-center col-span-2">
         <Link
           to="/landlord"
-          state={{ landlord: landlord, from: 'LandlordCard' }}
+          state={{ landlord: landlord, from: "LandlordCard" }}
           className="landlord-reviews-link text-gray-800 text-md "
         >
           <button
