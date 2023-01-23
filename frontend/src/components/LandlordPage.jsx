ce54010f8f155951f34da6ed1ea7109b5dfe8540
Jeff Pettis <jeffrey.pettis@gmail.com>
2023-01-23 14:56:39 -0800

    Added unit tests for LandlordCard, Login, and SearchPage

--- frontend/src/components/LandlordPage.jsx
+++ frontend/src/components/LandlordPage.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import AddReview from './AddReview.jsx';
-import ReviewDetails from './ReviewDetails.jsx';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import AddReview from "./AddReview.jsx";
+import ReviewDetails from "./ReviewDetails.jsx";
 
 const LandlordPage = () => {
   const location = useLocation();
@@ -10,31 +10,36 @@ const LandlordPage = () => {
   const [data, setData] = useState({ landlord: {}, reviews: [] }); // data.landlord, data.reviews
 
   useEffect(() => {
-    if (from === 'LandlordCard') {
-      fetch('/api/getlandlord/' + landlord._id)
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .then(console.log('data in fetch', data));
+    if (from === "LandlordCard") {
+      fetch("/api/getlandlord/" + landlord._id)
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .then(console.log("data in fetch", data));
     } else {
-      setData({ landlord })
-    }}, [])
+      setData({ landlord });
+    }
+  }, []);
 
   return (
     <div className="flex flex-col items-center py-2">
-    
       <h2 className="page-title py-4">{data.landlord.name}</h2>
 
-      <p className="label"> Main City:
+      <p className="label">
+        {" "}
+        Main City:
         {data.landlord.location}
       </p>
 
-      <p className="label">Rating:
-        {data.landlord.rating ? data.landlord.rating : 'N/A'}
+      <p className="label">
+        Rating:
+        {data.landlord.rating ? data.landlord.rating : "N/A"}
       </p>
 
       <p className="label label-rent-again mb-2">
         Would Rent Again:
-        {data.landlord.would_rent_again ? data.landlord.would_rent_again : 'N/A'}
+        {data.landlord.would_rent_again
+          ? data.landlord.would_rent_again
+          : "N/A"}
       </p>
 
       <div className="reviews">
@@ -42,9 +47,8 @@ const LandlordPage = () => {
           data.reviews.map((review) => (
             <ReviewDetails key={review._id} review={review} />
           ))}
-        <AddReview landlord={data.landlord}/>
+        <AddReview landlord={data.landlord} />
       </div>
-
     </div>
   );
 };
