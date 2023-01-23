fd018fa94ab762b0edcb5cd602c1ce909e1ce2bb
JamesJunJieHuang <108486354+JamesJunJieHuang@users.noreply.github.com>
2023-01-23 17:12:23 -0500

    update

--- backend/auth.js
+++ backend/auth.js
@@ -14,6 +14,7 @@ passport.use(
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
+    
     console.log('profile: ', profile)
       return done(null, profile);
     }
