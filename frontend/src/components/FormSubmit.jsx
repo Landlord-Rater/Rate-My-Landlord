3c4ed0650a314834358d0a4fd16bb27d08f65fbc
Jeff Pettis <jeffrey.pettis@gmail.com>
2023-01-28 12:14:27 -0800

    fixed modal close

--- frontend/src/components/FormSubmit.jsx
+++ frontend/src/components/FormSubmit.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function FormSubmit({ value, onClick }) {
   return (
     <button
+      data-bs-dismiss="modal"
       type="submit"
       className="w-full rounded bg-secondary text-primary hover:bg-opacity-90 hover:text-dark-purple transition font-semibold text-lg cursor-pointer p-1"
       value={value}
