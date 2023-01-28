3ce2e07331fd20b1016c033422267fff74f8d45e
michellesylee <michellebellelee@yahoo.com>
2023-01-28 11:38:54 -0800

    rerenders maps and reviews

--- frontend/src/components/LandlordPage.jsx
+++ frontend/src/components/LandlordPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, createContext } from "react";
 import { useLocation } from "react-router-dom";
 import AddReview from "./AddReview.jsx";
 import ReviewDetails from "./ReviewDetails.jsx";
@@ -6,6 +6,9 @@ import ModalReview from "./ModalReview.jsx";
 import ModalProperty from "./ModalProperty.jsx";
 import GMap from "./GoogleMaps.jsx";
 import GetGeocode from "./Geocoder.jsx";
+
+// create context for submission handler
+export const LandlordPageContext = createContext(null);
 
 const loadGoogleMapScript = (callback) => {
   if (
@@ -45,6 +48,13 @@ const LandlordPage = () => {
     }
   }, []);
 
+  // create a handler to update data on submission
+  const handleAddressSubmit = () => {
+    fetch("/landlords/" + landlord._id)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  };
+
   // const [lat, setLat] = useState("");
   // const [lng, setLng] = useState("");
 
@@ -57,50 +67,52 @@ const LandlordPage = () => {
   }, []);
 
   return (
-    <div className=" review-page-container bg-no-repeat bg-cover flex flex-col items-center py-3">
-      <div className="demoInfogrid grid grid-cols-2 w-80 bg-primary text-white rounded text-l font-semibold ">
-        <h2 className="page-title py-4 col-span-2 text-center text-xl ">
-          {data.landlord.name}
-        </h2>
-        <div className="labelsColumn pl-6 p-6 col-span-1">
-          <div className="label">Main City:</div>
-          <div className="label">Rating:</div>
-          <div className="label label-rent-again mb-2">Would Rent Again:</div>
-        </div>
-        <div className="valuesColumn p-6 col-span-1">
-          <div>{data.landlord.location}</div>
-          <div>{data.landlord.rating ? data.landlord.rating : "N/A"}</div>
-          <div>
-            {data.landlord.would_rent_again
-              ? data.landlord.would_rent_again
-              : "N/A"}
+    <LandlordPageContext.Provider value={{ handleAddressSubmit }}>
+      <div className=" review-page-container bg-no-repeat bg-cover flex flex-col items-center py-3">
+        <div className="demoInfogrid grid grid-cols-2 w-80 bg-primary text-white rounded text-l font-semibold ">
+          <h2 className="page-title py-4 col-span-2 text-center text-xl ">
+            {data.landlord.name}
+          </h2>
+          <div className="labelsColumn pl-6 p-6 col-span-1">
+            <div className="label">Main City:</div>
+            <div className="label">Rating:</div>
+            <div className="label label-rent-again mb-2">Would Rent Again:</div>
+          </div>
+          <div className="valuesColumn p-6 col-span-1">
+            <div>{data.landlord.location}</div>
+            <div>{data.landlord.rating ? data.landlord.rating : "N/A"}</div>
+            <div>
+              {data.landlord.would_rent_again
+                ? data.landlord.would_rent_again
+                : "N/A"}
+            </div>
           </div>
         </div>
+
+        <div className="button-container">
+          <ModalProperty />
+
+          <ModalReview />
+        </div>
+
+        <div className="reviews-container">
+          {data.reviews &&
+            data.reviews.map((review) => (
+              <ReviewDetails key={review._id} review={review} />
+            ))}
+          {console.log(data.reviews)}
+
+          {/* <AddReview landlord={data.landlord} /> */}
+        </div>
+
+        {/* <GetGeocode /> */}
+        <div className="App">
+          <br />
+          <br />
+          {!loadMap ? <div>Loading...</div> : <GMap props={data} />}
+        </div>
       </div>
-
-      <div className="button-container">
-        <ModalProperty />
-
-        <ModalReview />
-      </div>
-
-      <div className="reviews-container">
-        {data.reviews &&
-          data.reviews.map((review) => (
-            <ReviewDetails key={review._id} review={review} />
-          ))}
-        {console.log(data.reviews)}
-
-        {/* <AddReview landlord={data.landlord} /> */}
-      </div>
-
-      {/* <GetGeocode /> */}
-      <div className="App">
-        <br />
-        <br />
-        {!loadMap ? <div>Loading...</div> : <GMap props={data} />}
-      </div>
-    </div>
+    </LandlordPageContext.Provider>
   );
 };
 
